b9e01dbc53b5544a420f471abcdfa42eb3fc661f
David Ellis <isv.damocles@gmail.com>
2022-03-09 15:50:43 -0600

    0.9.46

--- Makefile
+++ Makefile
@@ -2,7 +2,8 @@ SHELL = /bin/bash
 
 .PHONY: install
 install:
-	curl https://arctype-downloads.s3-us-west-2.amazonaws.com/updates/Arctype-0.9.45.AppImage --output arctype
-	chmod +x arctype
+	curl https://arctype-downloads.s3-us-west-2.amazonaws.com/updates/Arctype-0.9.46.AppImage --output arctype
 	cp ./arctype /usr/local/bin/arctype
+	chmod 761 /usr/local/bin/arctype
+	chmod g+s /usr/local/bin/arctype
 	rm ./arctype
--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 set -vex
 
-echo "Arctype SQL Client\n\n" | sudo checkinstall --install=no -D --pkgversion=0.9.45 --pkglicense=Proprietary --maintainer=support@arctype.com --pkggroup="Arctype, Inc" --nodoc --deldesc=yes --delspec=yes --backup=no
+echo "Arctype SQL Client\n\n" | sudo checkinstall --install=no -D --pkgversion=0.9.46 --pkglicense=Proprietary --maintainer=support@arctype.com --pkggroup="Arctype, Inc" --nodoc --deldesc=yes --delspec=yes --backup=no
